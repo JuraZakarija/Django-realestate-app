3986881930e31dfa6253a8b9effda5b61d41a106
Jura Zakarija <zakarijaj@gmail.com>
2019-10-13 23:02:06 +0200

    Add search page

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
 
+from listings.choices import price_choices, bedroom_choices, state_choices
+
 # Create your views here.
 
 
@@ -11,7 +13,12 @@ def index(request):
     # Get all listings
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
-    context = {'listings': listings}
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
 
     return render(request, 'pages/index.html', context)
 
